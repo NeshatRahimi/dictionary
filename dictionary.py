15863c2191482d20ed1a02cf1af0cfce7f7339d9
NeshatRahimi <113875103+NeshatRahimi@users.noreply.github.com>
2022-09-19 12:42:39 +0430

    Update and rename dictionary to dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,64 @@
+import json
+from pathlib import Path
+
+import difflib
+from difflib import get_close_matches
+
+contents = Path('/Users/neshat/Downloads/dictionarydata.json').read_text()
+print(type(contents))
+
+words_dict = json.loads(contents)
+
+print(type(words_dict))
+
+def take_lookup():
+    
+    word = input('enter the word:')
+
+    for w in words_dict:
+        if word in words_dict:
+            print(words_dict[word])
+            break
+            
+        else:
+            guess = get_close_matches(word , words_dict)
+            word = input('which one did you mean? {}'.format(guess))
+        
+take_lookup()
+
+
+second
+
+import json
+from pathlib import Path
+
+import difflib
+from difflib import get_close_matches
+
+contents = Path('/Users/neshat/Downloads/dictionarydata.json').read_text()
+print(type(contents))
+
+words_dict = json.loads(contents)
+
+print(type(words_dict))
+
+def take_lookup():
+    
+    word = input('enter the word:')
+
+    for w in words_dict:
+
+        if word in words_dict:
+            print(words_dict[word])
+            break
+            
+        else:
+            guess = get_close_matches(word , words_dict)
+            if len(guess) < 1:
+                print("I don't know either. ask google")
+                break
+            else:
+                word = input('which one did you mean? {}'.format(guess))
+        
+take_lookup()
+
